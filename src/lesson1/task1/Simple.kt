3adf68e77a4e1265eb0c3a644ca4717a61899ea7
Konstantin-Erastov <konstantin.yerastov@bk.ru>
2021-11-02 00:22:24 +0300

    Исправил 1 урок

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,9 +65,9 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    return hours * 3600 + minutes * 60 + seconds
-}
+fun seconds(hours: Int, minutes: Int, seconds: Int) = hours * 3600 + minutes * 60 + seconds
+
+
 
 
 /**
@@ -77,13 +77,9 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
-    val x = sagenes * 48
-    val y = arshins * 16
-    val z = x + y + vershoks
-    val a = z * 4.445 / 100
-    return a
-}
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) = (sagenes * 48 + arshins * 16 + vershoks) * 4.445 / 100
+
+
 
 /**
  * Тривиальная (1 балл)
@@ -91,9 +87,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
-    return (deg + (min / 60.0) + (sec / 3600.0)) / 180.0 * PI
-}
+fun angleInRadian(deg: Int, min: Int, sec: Int) = (deg + (min / 60.0) + (sec / 3600.0)) / 180.0 * PI
 
 /**
  * Тривиальная (1 балл)
@@ -109,9 +103,9 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int {
-    return number % 1000 / 100
-}
+fun thirdDigit(number: Int) = number % 1000 / 100
+
+
 
 /**
  * Простая (2 балла)
