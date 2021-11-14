1afdda09e414150a28f817c0eb15249b95675051
Konstantin-Erastov <konstantin.yerastov@bk.ru>
2021-11-14 22:31:17 +0300

    5 урок findCheapestStuff

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -187,8 +187,8 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
     var cost = 0.0
     var tag = ""
     for ((i) in stuff)
-        if ((stuff[i]?.first == kind) && ((cost == 0.0) || (cost > (stuff[i]?.second?: return tag)))) {
-            cost = (stuff[i]?.second?: return tag)
+        if ((stuff[i]?.first == kind) && ((cost == 0.0) || (cost > (stuff[i]?.second?: return null)))) {
+            cost = (stuff[i]?.second?: return null)
             tag = i
         }
 if (tag == "") {
