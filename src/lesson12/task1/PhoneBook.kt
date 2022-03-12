9fa886154caaed47f7863fac9294193dfd005d4a
Konstantin-Erastov <konstantin.yerastov@bk.ru>
2022-03-13 01:50:51 +0300

    PhoneBook 12 урок

--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -2,6 +2,7 @@
 
 package lesson12.task1
 
+
 /**
  * Класс "Телефонная книга".
  *
@@ -18,13 +19,19 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    private val book = mutableMapOf<String, MutableSet<String>>()
+
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean {
+        if (book.containsKey(name)) return false
+        book[name] = mutableSetOf()
+        return true
+    }
 
     /**
      * Убрать человека.
@@ -32,7 +39,11 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean {
+        if (!book.containsKey(name)) return false
+        book.remove(name)
+        return true
+    }
 
     /**
      * Добавить номер телефона.
@@ -41,7 +52,13 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean {
+        if (!book.containsKey(name)) return false
+        if (humanByPhone(phone) != null) return false
+        book[name]!!.add(phone)
+        return true
+
+    }
 
     /**
      * Убрать номер телефона.
@@ -49,24 +66,56 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean {
+        if (!book.containsKey(name)) return false
+        val phones = book[name]!!
+        if (phone !in phones) return false
+        phones.remove(phone)
+        return true
+    }
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> {
+        if (!book.containsKey(name)) return setOf()
+        return book[name]!!.toSet()
+    }
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? {
+        book.forEach {
+            val phones = it.value
+            if (phones.contains(phone)) return it.key
+        }
+        return null
+    }
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        other as PhoneBook
+        if (other.book.size != book.size) return false
+        book.forEach {
+            if (!other.book.containsKey(it.key)) return false
+            val phones = it.value
+            val otherPhones = other.book[it.key]!!
+            if (phones.size != otherPhones.size) return false
+            phones.forEach { it ->
+                if (!otherPhones.contains(it)) return false
+            }
+        }
+        return true
+    }
+
+    override fun hashCode(): Int = book.hashCode()
 }
